64af607cfa1953b5c9b0618e184c3b3246d01f8d
Diddy <willips@gmail.com>
2023-05-10 00:00:39 -0400

    TEST

--- vue-app/src/views/blist.vue
+++ vue-app/src/views/blist.vue
@@ -2,11 +2,11 @@
   <div class="content-container">
     <div class="content-title-group not-found">
       <h2 class="title">Product Wish List</h2>
-      <p>
-        This  was created to help represent a fundamental app written
-        with Vue. The shopping theme is used throughout the app.
-      </p>
-      <br />
+     <p>
+      TESTING
+
+    </p>
+          <br />
       <h2 class="title">Resources</h2>
       <ul>
         <li>
