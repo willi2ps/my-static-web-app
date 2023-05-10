b8a25cda4ca7f7b7a6d7a913ba1ef660c2812476
Diddy <willips@gmail.com>
2023-05-09 23:50:02 -0400

    testing

--- vue-app/src/components/nav-bar.vue
+++ vue-app/src/components/nav-bar.vue
@@ -3,7 +3,8 @@
     <p class="menu-label">Menu</p>
     <ul class="menu-list">
       <router-link to="/products">Products</router-link>
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">Abouuxt</router-link>
+      <router-link to="/blist">Black Listing</router-link>
     </ul>
   </nav>
 </template>
--- vue-app/src/router.js
+++ vue-app/src/router.js
@@ -30,6 +30,15 @@ export default new Router({
         import(/* webpackChunkName: "about" */ './views/about.vue'),
     },
     {
+      path: '/blist',
+      name: 'blist',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () =>
+        import(/* webpackChunkName: "about" */ './views/blist.vue'),
+    },
+    {
       path: '*',
       component: PageNotFound,
     },
